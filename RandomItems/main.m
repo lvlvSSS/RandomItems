4c75fc97d9cfc2fc7080740a7eb43dbbc45c699d
lvlvSSS <nelson.lv.007@outlook.com>
2020-07-26 11:52:47 +0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by 吕浩林 on 2020/7/26.
+//  Copyright © 2020 nelson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
